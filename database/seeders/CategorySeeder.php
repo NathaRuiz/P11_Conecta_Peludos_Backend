d7aadab26fb78e3887e4bb87b398657647bce280
NathaRuiz <nruizm98@gmail.com>
2024-03-13 18:55:37 +0100

    create some seeders and install laravel breeze api with sanctum

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Perros',
+            'Gatos',
+            'Pájaros',
+            'Conejos',
+            'Otros'
+        ];
+
+        foreach ($categories as $categoryName) {
+            Category::create(['name' => $categoryName]);
+        }
+    }
+}
